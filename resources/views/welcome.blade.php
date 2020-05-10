98653776619b0882ca68f6d7e0c7fd9aa3b7000b
kibria <studentgolamkibria@gmail.com>
2020-05-10 17:57:37 +0600

    'merce'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   ecomerce
+                   hhhhhh
                 </div>
 
                 <div class="links">
