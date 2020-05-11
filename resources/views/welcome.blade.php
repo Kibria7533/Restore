04a7475e3525e5db404f25a7121ab466a94d6668
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-05-11 15:57:38 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   hatim
+                   CSE
                 </div>
 
                 <div class="links">
